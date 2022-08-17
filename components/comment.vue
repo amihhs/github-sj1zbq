b977213c000885df88981b4b44863207e7043834
张建华 <1804728126@qq.com>
2022-08-17 11:53:02 +0800

    init

--- components/comment.vue
+++ components/comment.vue
@@ -1,18 +1,7 @@
-<script setup lang="ts">
-import { useWindowScroll } from '@vueuse/core';
-const isVisibleHeader = ref(false);
-// if (isClient) {
-//   const { y } = useWindowScroll()
-//   watch(y, (newValue, oldValue) => {
-//     if (newValue > oldValue && newValue > 200)
-//       isVisibleHeader.value = true
-//     else if (newValue < oldValue)
-//       isVisibleHeader.value = false
-//   })
-// }
-</script>
 <template>
-  <div :class="{ 'header-hidden': isVisibleHeader }">component one</div>
+  <div >component one</div>
   <!-- component comment-->
-  <div :class="{ 'header-hidden': isVisibleHeader }" />
+  <div  />
+   <!-- component comment-->
+  <div >aa</div>
 </template>
